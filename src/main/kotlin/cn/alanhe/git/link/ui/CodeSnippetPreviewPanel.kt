2a4b435a0edad343930faa21e39d2bcf61c1e2f9
Alan He <qianghe421@gmail.com>
2025-09-22 18:50:11 +0800

    Add debug button to code snippet preview for internal use

--- src/main/kotlin/cn/alanhe/git/link/ui/CodeSnippetPreviewPanel.kt
+++ src/main/kotlin/cn/alanhe/git/link/ui/CodeSnippetPreviewPanel.kt
@@ -142,6 +142,8 @@ class CodeSnippetPreviewPanel(private val project: Project) : SimpleToolWindowPa
         // Replace placeholders with actual values
         val watermark = settings.codeImageWatermark
         val showWatermark = watermark.isNotEmpty()
+        val application = ApplicationManager.getApplication()
+        val debugButton = if (application.isInternal || application.isUnitTestMode) DEBUG_BUTTON_HTML else ""
 
         return template
             .replace("%LANGUAGE%", language)
@@ -150,6 +152,7 @@ class CodeSnippetPreviewPanel(private val project: Project) : SimpleToolWindowPa
             .replace("%SHOW_REMOTE%", if (useRemote) "block" else "none")
             .replace("%WATERMARK%", escapeHtml(watermark))
             .replace("%WATERMARK_DISPLAY%", if (showWatermark) "block" else "none")
+            .replace("%DEVTOOLS_BUTTON%", debugButton)
     }
 
     private fun escapeHtml(text: String): String {
@@ -403,3 +406,11 @@ class CodeSnippetPreviewToolWindowFactory : ToolWindowFactory {
         toolWindow.contentManager.addContent(content)
     }
 }
+
+private val DEBUG_BUTTON_HTML = """
+            <button id="debugButton" data-tooltip="Open DevTools (Ctrl+Shift+I)" style="margin-left: 8px;" onclick="openDebugTools()">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 8h-2.81c-.45-.78-1.07-1.45-1.82-1.96L17 4.41 15.59 3l-2.17 2.17C12.96 5.06 12.49 5 12 5s-.96.06-1.42.17L8.41 3 7 4.41l1.62 1.63C7.88 6.55 7.26 7.22 6.81 8H4v2h2.09c-.05.33-.09.66-.09 1v1H4v2h2v1c0 .34.04.67.09 1H4v2h2.81c1.04 1.79 2.97 3 5.19 3s4.15-1.21 5.19-3H20v-2h-2.09c.05-.33.09-.66.09-1v-1h2v-2h-2v-1c0-.34-.04-.67-.09-1H20V8zm-6 8h-4v-2h4v2zm0-4h-4v-2h4v2z" fill="currentColor"/>
+                </svg>
+            </button>
+""".trimIndent()
